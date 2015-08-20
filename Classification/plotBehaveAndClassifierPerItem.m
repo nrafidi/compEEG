e5d0d392e4760b7d5d49245628a19fa2226abbad
Nicole <nicole.rafidi@gmail.com>
2015-08-20 15:28:57 -0400

    Code for generating folders of single-trial plots

--- Classification/plotBehaveAndClassifierPerItem.m
+++ Classification/plotBehaveAndClassifierPerItem.m
@@ -1,20 +1,30 @@
 % Analyize and summarize behavioral data
 
 %
-subjects = {'AA'};%, 'BB', 'DD', 'EE', 'F', 'GG', 'HH', 'JJ', ...
-%     'K', 'M', 'N', 'O', 'R', 'S', 'T', 'U', 'V', 'X', 'Y', 'Z'};
+subjects = { 'AA', 'BB', 'DD', 'EE', 'F', 'GG', 'HH', 'JJ', ...
+    'K', 'M', 'N', 'O', 'R', 'S', 'T', 'U', 'V', 'X', 'Y', 'Z'};
 numSub = length(subjects);
 item = 1;
 
 behaveDataRoot = '/Users/nrafidi/Documents/MATLAB/compEEG-data/behavioral/';
+figureRoot = '/Users/nrafidi/Documents/MATLAB/compEEG-data/results/figures/';
 stimRoot = '/Users/nrafidi/Documents/MATLAB/compEEG-stim/';
 fid = fopen([stimRoot 'KR_test.txt']);
 swahili = textscan(fid, '%s');
 swahili = swahili{1};
 
-potentialResponses = [0 0 0 0; 0 0 0 1; 0 0 1 0; 0 0 1 1; 0 1 0 0; 0 1 0 1; ...
-    0 1 1 0; 0 1 1 1; 1 0 0 0; 1 0 0 1; 1 0 1 0; 1 0 1 1; 1 1 0 0; 1 1 0 1; ...
-    1 1 1 0; 1 1 1 1];
+potentialResponses = [0 0 0 1; 0 0 1 1;  0 1 1 1; 1 1 1 1];
+respNames = {'R4', 'R3R4', 'R2R3R4', 'R1R2R3R4'};
+
+for r = 1:length(respNames)
+    if ~exist([figureRoot respNames{r} '_corr'], 'dir')
+        mkdir([figureRoot respNames{r} '_corr']);
+    end
+    if ~exist([figureRoot respNames{r} '_inc'], 'dir')
+        mkdir([figureRoot respNames{r} '_inc']);
+    end
+end
+
 numPotResp = size(potentialResponses, 1);
 krTrajByResponse = cell(numPotResp, 2);
 for s = 1:numSub
@@ -32,92 +42,54 @@ for s = 1:numSub
     corrItems =find(krLabels);
     incItems = find(~krLabels);
     
-    figure;
-    plot(1:4, krTraj(corrItems(1),:), 'b');
-    hold on;
-    for i = 1:4
-        if responseTraj(corrItems(1), i)
-            scatter(i, krTraj(corrItems(1),i), 'MarkerEdgeColor', 'blue', ...
-                'Marker', 'o', 'SizeData', 100);
-        else
-            scatter(i, krTraj(corrItems(1),i), 'MarkerEdgeColor', 'blue', ...
-                'Marker', 'x', 'SizeData', 100);
+    for cItem = 1:length(corrItems)
+        trajID = responseTraj(corrItems(cItem,:), :);
+        rowID = ismember(potentialResponses, trajID, 'rows');
+        if any(rowID)
+            f = figure;
+            plot(1:4, krTraj(corrItems(cItem),:), 'b');
+            hold on;
+            for i = 1:4
+                if trajID(i)
+                    scatter(i, krTraj(corrItems(cItem),i), 'MarkerEdgeColor', 'blue', ...
+                        'Marker', 'o', 'SizeData', 100);
+                else
+                    scatter(i, krTraj(corrItems(cItem),i), 'MarkerEdgeColor', 'blue', ...
+                        'Marker', 'x', 'SizeData', 100);
+                end
+            end
+            xlim([0.9, 4.1]);
+            ylim([-0.1 1.1]);
+            title([sub ' ' subSwahili{corrItems(cItem)}]);
+            hold off;
+            saveas(f, [figureRoot respNames{rowID} '_corr/' sub '_' subSwahili{corrItems(cItem)} '.png']);
+            close(f);
         end
     end
-    xlim([0.9, 4.1]);
-    ylim([-0.1 1.1]);
-    title(subSwahili{corrItems(1)});
-    hold off;
     
-    figure;
-    plot(1:4, krTraj(incItems(2),:), 'r');
-    hold on;
-    for i = 1:4
-        if responseTraj(incItems(2), i)
-            scatter(i, krTraj(incItems(2),i), 'MarkerEdgeColor', 'red', ...
-                'Marker', 'o', 'SizeData', 100);
-        else
-            scatter(i, krTraj(incItems(2),i), 'MarkerEdgeColor', 'red', ...
-                'Marker', 'x', 'SizeData', 100);
+    for iItem = 1:length(incItems)
+        trajID = responseTraj(incItems(iItem,:), :);
+        rowID = ismember(potentialResponses, trajID, 'rows');
+        if any(rowID)
+            f = figure;
+            plot(1:4, krTraj(incItems(iItem),:), 'r');
+            hold on;
+            for i = 1:4
+                if trajID(i)
+                    scatter(i, krTraj(incItems(iItem),i), 'MarkerEdgeColor', 'red', ...
+                        'Marker', 'o', 'SizeData', 100);
+                else
+                    scatter(i, krTraj(incItems(iItem),i), 'MarkerEdgeColor', 'red', ...
+                        'Marker', 'x', 'SizeData', 100);
+                end
+            end
+            xlim([0.9, 4.1]);
+            ylim([-0.1 1.1]);
+            title([sub ' ' subSwahili{incItems(iItem)}]);
+            hold off;
+            saveas(f, [figureRoot respNames{rowID} '_inc/' sub '_' subSwahili{corrItems(cItem)} '.png']);
+            close(f);
         end
     end
-    xlim([0.9, 4.1]);
-    ylim([-0.1 1.1]);
-    title(subSwahili{incItems(2)});
-    hold off;
-%     
-%     for r = 1:numPotResp
-%         indOfResp = ismember(responseTraj, potentialResponses(r,:), 'rows');
-%         
-%         krTrajByResponse{r, 1} = cat(1, krTrajByResponse{r, 1}, krTraj(indOfResp & krLabels, :));
-%         krTrajByResponse{r, 2} = cat(1, krTrajByResponse{r, 2}, krTraj(indOfResp & ~krLabels, :));
-%     end
     
 end
-% %%
-% titles = {'None', 'R4', 'R3', 'R3, R4', 'R2', 'R2, R4', 'R2, R3', 'R2, R3, R4', ...
-%     'R1', 'R1, R4', 'R1, R3', 'R1, R3, R4', 'R1, R2', ...
-%     'R1, R2, R4', 'R1, R2, R3', 'R1, R2, R3, R4'};
-% numPlots = sqrt(numPotResp);
-% numTot = 0;
-% for traj = 1:numPotResp
-%     numCorrItems = size(krTrajByResponse{traj, 1}, 1);
-%     numIncItems = size(krTrajByResponse{traj, 2}, 1);
-%     numTot = numTot + numCorrItems + numIncItems;
-%     titleForPlot = sprintf([titles{traj} ...
-%         '\nC = %d I = %d'], numCorrItems, numIncItems);
-%     
-%     subplot(numPlots, numPlots, traj);
-%     hold on;
-%     if numCorrItems > 0
-%         plot(1:4, mean(krTrajByResponse{traj, 1}, 1), 'b');
-%         if numCorrItems > 1
-%             errorbar(1:4, mean(krTrajByResponse{traj, 1}, 1), ...
-%                 std(krTrajByResponse{traj, 1}, 1), 'b');
-%         end
-%     else
-%         plot(1:4, zeros(1, 4), 'b');
-%     end
-%     
-%     if numIncItems > 0
-%         plot(1:4, mean(krTrajByResponse{traj, 2}, 1), 'r');
-%         if numIncItems > 1
-%             errorbar(1:4, mean(krTrajByResponse{traj, 2}, 1), ...
-%                 std(krTrajByResponse{traj, 2}, 1), 'r');
-%         end
-%     else
-%         plot(1:4, zeros(1, 4), 'r');
-%     end
-%     if traj == 13
-%         legend({'Remembered', 'Forgotten'}, 'Location', 'north');
-%     end
-%     title(titleForPlot);
-%     if traj == numPotResp
-%         xlabel('Round');
-%     end
-%     if traj == 1
-%         ylabel('Competition Classifier Output');
-%     end
-%     xlim([0.9 4.1]);
-%     ylim([-0.1 1.1]);
-% end
