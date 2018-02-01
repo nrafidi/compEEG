bea238dd6441d11222d5a767d3424ea6759df932
Nicole <nicole.rafidi@gmail.com>
2018-02-01 08:17:19 -0500

    updated bash script for compeeg perm tests

--- Classification/bash_CompEEG.sh
+++ Classification/bash_CompEEG.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-subjectList=(AAA BBB CCC DDD EEE FFF GGG HHH III JJJ KKK MM OO PP QQ RR SS TT WW YY)
+subjectList=(AA BB CC DD EE F FF GG H HH I J JJ K L M N O P R S T U V W X Y Z)
 
-winList=(1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30 31 32 33 34 35 36 37 38 39 40)
+winList=(1 2 3 4 5 6 26 27 28 29 30 31 32 33 34 35 36 37 38 39 40)
 
 
 for i in ${subjectList[@]}
